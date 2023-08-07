a7df238321f2e2a410de1c91913cb01144ed6027
paritoshparashar <parasharparitosh2003@gmail.com>
2023-08-07 20:32:26 +0000

    Implement Step 1 of the TOP Assignment, i.e. Add all the basic math functions

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ for (let i = 0; i < 19 ; i++) {
     }
     numbers.appendChild(number);
 }
+
+function sum (a, b){
+    return a+b;
+}
+function subtract (a, b){
+    return a-b;
+}
+function multiply (a, b){
+    return a*b;
+}
+function divide (a, b){
+    return a/b;
+}
+function mod (a, b){
+    return a % b;
+}
