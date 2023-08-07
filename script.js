353ed2968476f4647dabb18f2d970c187c9210a5
paritoshparashar <parasharparitosh2003@gmail.com>
2023-08-07 21:42:05 +0000

    Add the numbers from 0 to 9 on the respective divs, and change the background colour

--- script.js
+++ script.js
@@ -2,20 +2,38 @@
 const numbers = document.getElementById('numbers');
 let parentWidthtNumbers = numbers.offsetWidth;
 let parentHeightNumbers = numbers.offsetHeight;
-
-for (let i = 0; i < 19 ; i++) {
-      
+let j = 9;
+for (let i = 1; i < 20 ; i++) {
+    
     let number = document.createElement('div');
-    number.className = ''+(i+1);
+    number.className = ''+(i);
     number.style.width = (parentWidthtNumbers-30)/4 + 'px';
     number.style.height =( parentWidthtNumbers-30)/4 + 'px';
     number.style.backgroundColor = "black";
     number.style.marginTop = '10px';
     number.style.borderRadius = "50%";
-    if (i == 18) {
+    number.style.color = "white";
+    number.style.display = "flex";
+    number.style.justifyContent = "center";
+    number.style.alignItems = "center";
+    number.style.fontSize = "25px";
+    number.style.fontWeight = "bold";
+
+    if (i >=5 && i<=17 ) {
+        if (i%4 != 0) {     
+        number.textContent = ''+ j;
+        --j;
+        }
+        
+    }
+
+    if (i == 19) {
         number.style.flexGrow = "0.8";
         number.style.borderRadius = "40px";
+        number.textContent = "=";
+        number.style.fontSize = "50px";
     }
+
     numbers.appendChild(number);
 }
 //Implementing Calculator From here
